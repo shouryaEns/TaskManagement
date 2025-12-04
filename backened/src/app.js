9391bd6fcd2560f796565f9a244b7fc3ce21afca
Developer <shourya.saxena@ens.enterprises>
2025-12-05 03:29:25 +0530

    taskflow health api

--- backened/src/app.js
+++ backened/src/app.js
@@ -18,8 +18,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(morgan('combined'));
-app.use('/',(req, res) => {
-  res.send('API is running...');
+app.get('/', (req, res) => {
+  res.json({ message: 'TaskFlow Pro API is running' });
 });
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
