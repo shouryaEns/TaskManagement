1bbcfb53bc1c32a5a5ed314fcdc758dd1ff81ecd
Developer <shourya.saxena@ens.enterprises>
2025-12-05 02:35:49 +0530

    added health api

--- backened/src/app.js
+++ backened/src/app.js
@@ -18,7 +18,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(morgan('combined'));
-
+app.use('/',(req, res) => {
+  res.send('API is running...');
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use(errorHandler);
